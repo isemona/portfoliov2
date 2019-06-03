94b66f1cb67d4b8d51cfd392261203ea30a4468e
Semona <semona.igama@gmail.com>
2019-06-02 19:13:56 -0700

    updated about and blog

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,32 @@
 <template>
 <div>
-  <h1 id="notes">
-      <ul id="navlist">
+  <h1>
+      <p>I am told a <strong>whippersnapper</strong> jokingly equates to a millenial. Merriam-Webster defines a 
+        whippersnapper to be: <em>a young person who annoys older people by being very confident and acting like 
+        someone important</em>. It is a word often used to describe millenials taking over Silicon Valley, 
+        but I'd like to think we are just a generation shaped by the ones before us. I'd like to think that a 
+        <i>whippersnapper</i> is someone with creative solutions to real-world problems; albeit young and 
+        inexperienced, they are bold and full of ideas to change the world around them.</p>
 
-        <li><a href="/">Home</a></li>
-        <li><a href="/wsnotes.html">Notes</a></li>
-        <li><a href="/wsprojects.html">Projects</a></li>
-      </ul>
+      <p>As a Scientist/Public Health Worker turned Software Developer, I am most interested in the intersection
+          of healthcare and technology. I moved to the Bay Area from an island in the Pacific Ocean called Saipan. 
+          Here, I volunteered at the local hospital helping patients to understand and educate/manage diabetes and 
+          other preventable illnesses. I also volunteered at a women's shelter; where I taught English to women 
+          from different countries looking to find work on the island and transition to healthier lives. In 2013, 
+          I moved to San Francisco, where I volunteered as a public health intern with AmeriCorps under 
+          San Francisco Department of Public Health. Again, I have seen a disconnect with how our current 
+          healthcare system operates; a system more focused on the end result and not enough on prevention 
+          and education.</p>
+
+      <p>I aim to research and contribute to current technological efforts out there -- efforts that help to 
+          improve heathcare at its current state. I love collaborating with hackers, bioengineers, researchers, 
+          clinicians, and even citizen scientists -- the very heroes I draw inspiration from. Exposure from groups 
+          like BioCurious and events like Makerfaire that I finally realized this.</p>
+
+      <p>I'm the curious type. I love to learn from other people and adapt to my environment. 
+          I see oportunities all around me and do my best to seize everyone of them; I am amazed and enjoy how 
+          these spontaneous collaborations pan out. I love working with diverse teams and feel like I have much to 
+          give and learn from them as well. Energtic/motivated team fuel me to accompish meaningful goals.</p>
   </h1>
-        <!-- aka mute
-        -use this to link lessons <a href = "http://imgs.xkcd.com/comics/python.png"><img </a>
-        -tab and shift tab help indent blocks of text 
-        -->
-
-    <img class="displayed" src = "wsimages\semona.jpg" alt= "me on a hiking trip" border=5/>
-<h5>
-      <p>I am told a <strong>whippersnapper</strong> jokingly equates to a millenial. Merriam-Webster defines a whippersnapper to be: <em>a young person who annoys older people by being very confident and acting like someone important</em>. It is a word often used to describe the millenials taking over Silicon Valley, but I'd like to think we are just a generation shaped by the ones before us. I'd like to think that a <i>whippersnapper</i> is someone with creative solutions to real world problems; albeit young and inexperienced and more like bold and full of ideas to change the world around them.</p>
-
-      <p>I am most interested in how technology can be incorporated in health. I moved from a secluded island in the Pacific Ocean called Saipan. Here, I volunteered at the local hospital and women's shelter; working closely with patients and women dealing with diabetes and other preventable illnesses. Since then, I have moved to San Francisco, where I did some public health work with AmeriCorps under Department of Public Health. Again, I have seen a disconnect with how healthcare operates; a system more focused on the end result and not heavily on preventative care.</p>
-
-      <p>I aim to research and contribute to current technological efforts out there -- efforts that help to improve heathcare at its current state. I love collaborating with hackers, bioengineers, researchers, clinicians, and even citizen scientists -- the very heroes I draw inspiration from; it was at BioCurious and Makerfaire that I finally realized this.</p>
-
-      <p>I'm also a very curious person. I love to learn from other people and adapt to my environment. I see oportunities all around me and do my best to seize everyone of them. I love working with diverse teams and feel like I have much to learn from them. I feed from their energy which fuels me to accompish meaningful goals.</p>
-</h5>
 </div>
 </template>
